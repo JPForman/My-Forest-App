490074f980dfdd5e426ebb0bfcdd7ea1c2c5212a
Joshua Forman <cyborgforman@gmail.com>
2020-04-25 15:23:47 -0700

    add calendar mountain button and calendar

---
+++ src/components/Calendar.js
@@ -0,0 +1,15 @@
+import React, { Component, useState } from 'react';
+import CalendarReact from 'react-calendar';
+
+
+function Calendar() {
+
+  return (
+    <div>
+      <div className="calendarDiv">
+        <CalendarReact />
+      </div>
+    </div>
+  );
+}
+export default Calendar; 
--- src/components/MyForest.js
+++ src/components/MyForest.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import Calendar from './Calendar';
 
 //style sheets
 import '../css/myForest.css'
 
 function MyForest() {
+
+  const [calendarDisplay, setCalendarDisplay] = useState('none');
+
+  function hideCalendar() {
+    if (calendarDisplay === 'block') {
+      setCalendarDisplay('none'); }
+      else if(calendarDisplay === 'none') {
+        setCalendarDisplay('block');
+      }
+    }
     
     return (
       <div className='myForest'>
@@ -11,7 +22,11 @@ function MyForest() {
         <div className='sky'></div>
         <div className='sun'></div>
         <div className='ground'></div>
-       
+        <div className="mountain" onClick={hideCalendar}></div>
+
+        <div className='calendarReact' style={{display: `${calendarDisplay}`}}>
+          <Calendar className='calendar' />
+        </div>
       </div>
     );
 }
